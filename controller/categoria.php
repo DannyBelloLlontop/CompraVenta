55ea8c69f9f68006d3853bfc4698f6e9ac160cd3
DannyBelloLlontop <daniel.bello.llontop@gmail.com>
2023-01-11 21:55:26 -0500

    crud mantenimiento categoria

--- controller/categoria.php
+++ controller/categoria.php
@@ -22,9 +22,10 @@
             $data=Array();
             foreach($datos as $row){
                 $sub_array = array();
-                $sub_array = $row["cat_nom"];
-                $sub_array = "Editar";
-                $sub_array = "Eliminar";
+                $sub_array[] = $row["CAT_NOM"];
+                $sub_array[] = $row["FECH_CREA"];
+                $sub_array[] = '<button type="button" onclick="editar('.$row["CAT_ID"].')" id="'.$row["CAT_ID"].'" class="btn btn-warning btn-icon waves-effect waves-light"><i class="ri-edit-line"></i></button>';
+                $sub_array[] = '<button type="button" onclick="eliminar('.$row["CAT_ID"].')" id="'.$row["CAT_ID"].'" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-delete-bin-5-line"></i></button>';
                 $data[] = $sub_array;
             }
 
@@ -41,9 +42,9 @@
             $datos=$categoria->get_categoria_x_cat_id($_POST["cat_id"]);
             if(is_array($datos)==true and count($datos)>0){
                 foreach($datos as $row){
-                    $output["cat_id"]=$row["cat_id"];
-                    $output["suc_id"]=$row["suc_id"];
-                    $output["cat_nom"]=$row["cat_nom"];
+                    $output["CAT_ID"]=$row["CAT_ID"];
+                    $output["SUC_ID"]=$row["SUC_ID"];
+                    $output["CAT_NOM"]=$row["CAT_NOM"];
                 }
                 echo json_encode($output);
             }
